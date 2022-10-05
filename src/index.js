3b92347228fcb6ef097595fb8d2400dd735096ae
oliveiraviniciuss <vi.o@hotmail.com>
2022-10-04 21:41:23 -0300

    feat: Initial commit - Structuring the project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const app = require('./server')
+require('dotenv').config()
+
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+  console.log(`Server listening in ${port} on development env`);
+});

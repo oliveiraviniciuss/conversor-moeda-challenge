8ce557688200a7cab1f37d9a2373929d8041f516
oliveiraviniciuss <vi.o@hotmail.com>
2022-10-10 16:03:50 -0300

    remove unused code

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,8 +9,6 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('../swagger/swagger.json')
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
-// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 app.use('/conversion', conversion)
 app.use('/healthcheck', healthCheck)
 app.use('/', root)

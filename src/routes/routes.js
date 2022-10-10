be15ab6024b57b4f45bdcdf0224dd049cb99e687
oliveiraviniciuss <vi.o@hotmail.com>
2022-10-10 14:24:49 -0300

    lint-fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,12 +5,12 @@ const conversion = require('./conversion')
 const healthCheck = require('./healthCheck')
 const root = require('./root')
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger/swagger.json');
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../swagger/swagger.json')
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-//app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 app.use('/conversion', conversion)
 app.use('/healthcheck', healthCheck)
 app.use('/', root)

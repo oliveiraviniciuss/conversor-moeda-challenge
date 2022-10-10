a674022943001103eadd2d9a6611db7a58bede3c
oliveiraviniciuss <vi.o@hotmail.com>
2022-10-10 14:24:18 -0300

    Adding swagger

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const app = express()
 
+const app = express()
 const conversion = require('./conversion')
 const healthCheck = require('./healthCheck')
 const root = require('./root')
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger/swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 app.use('/conversion', conversion)
 app.use('/healthcheck', healthCheck)
 app.use('/', root)
+
 module.exports = app
